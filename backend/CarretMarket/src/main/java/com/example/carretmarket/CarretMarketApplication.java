970db5b2a06206b1e0855acb74af9fd67415ab4b
limsangun <tkrnfldlekd@gmail.com>
2023-06-19 22:23:14 +0900

    test

---
+++ backend/CarretMarket/src/main/java/com/example/carretmarket/CarretMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.carretmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarretMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarretMarketApplication.class, args);
+    }
+
+}
---
+++ backend/CarretMarket/src/test/java/com/example/carretmarket/CarretMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carretmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarretMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
